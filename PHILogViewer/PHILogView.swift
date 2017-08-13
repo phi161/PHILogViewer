8fa7ddf724f2b35d43434dabc01c8c1f7a6a78fa
phi161 <phi161@users.noreply.github.com>
2017-08-13 22:07:35 +0300

    Add a xib for the log view and a UIView subclass to manage it

---
+++ PHILogViewer/PHILogView.swift
@@ -0,0 +1,13 @@
+//
+//  PHILogView.swift
+//  PHILogViewer
+//
+//  Created by phi161 on 13/08/2017.
+//  Copyright © 2017 phi161. All rights reserved.
+//
+
+import UIKit
+
+class PHILogView: UIView {
+    @IBOutlet weak var textView: UITextView!
+}
