55dd49178cc10c31ace3711aa0d6b4c029d6eca6
phi161 <phi161@users.noreply.github.com>
2019-07-16 19:45:35 +0300

    feat(logging): log message when deleting objects

--- PHILogViewer/MasterViewController.swift
+++ PHILogViewer/MasterViewController.swift
@@ -86,6 +86,7 @@ class MasterViewController: UITableViewController {
 
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            PHILogger.log("deleted object at \(indexPath.row)")
             objects.remove(at: indexPath.row)
             tableView.deleteRows(at: [indexPath], with: .fade)
         } else if editingStyle == .insert {
